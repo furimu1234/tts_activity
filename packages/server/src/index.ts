d3efd7f66608faa38013eba2c798416665e63fd1
furimu <furimu@gmail.com>
2025-06-01 17:44:23 +0900

    もうわかんない

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,10 @@
+import { type UUID, randomUUID } from 'node:crypto';
 import { serve } from '@hono/node-server';
 import { createNodeWebSocket } from '@hono/node-ws';
 import * as dotenv from 'dotenv';
 import { Hono, type MiddlewareHandler } from 'hono';
 import api from './routes/api';
+import { websocket } from './websocket';
 
 // .envファイルを読み込む
 dotenv.config({ path: '../../.env' });
@@ -17,55 +19,33 @@ const hostFilter: MiddlewareHandler = async (c, next) => {
 
 const app = new Hono();
 app.use('*', hostFilter);
-export const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app });
+const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app });
 
-app.route('/', api);
+app.route('/api', api);
 
-app.post('/api/token', async (c) => {
-	// リクエストボディの取得
-	const body = await c.req.json();
-
-	// URLSearchParamsを使ってトークンリクエストのボディを構築
-	const tokenBody = new URLSearchParams({
-		client_id: process.env.VITE_CLIENT_ID!,
-		client_secret: process.env.CLIENT_SECRET!,
-		grant_type: 'authorization_code',
-		code: body.code,
-	});
-
-	// Discord APIにPOSTリクエストを送る
-	const response = await fetch(
-		`${process.env.VITE_DISCORD_API_BASE}/oauth2/token`,
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			body: tokenBody,
-		},
-	);
-
-	// レスポンスをJSONとして取得
-	const data = await response.json();
-	if (response.status !== 200) {
-		console.error(data);
-
-		return c.json(data, 500); // エラーレスポンスを返す
-	}
-
-	const { access_token } = data;
-
-	// アクセストークンをJSONで返す
-	return c.json(
-		{ access_token },
-		{ headers: { 'Access-Control-Allow-Origin': '*' } },
-	);
-});
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!');
 });
 
+app.get(
+	'/api/ws',
+	upgradeWebSocket((c) => ({
+		onOpen: (event, ws) => {
+			const clientId: UUID = randomUUID();
+			websocket.clients[clientId] = { client: ws, channelId: '' };
+			ws.send(JSON.stringify({ isSetClientId: true, clientId: clientId }));
+		},
+		onMessage: (event, ws) => {
+			const data = JSON.parse(event.data.toString());
+			const client = websocket.clients[data.clientId].client;
+			websocket.clients[data.clientId] = {
+				channelId: data.channelId,
+				client: client,
+			};
+		},
+	})),
+);
 
 app.notFound((c) => {
 	const path = c.req.url; // リクエストされたパスを取得
--- packages/server/src/routes/api.ts
+++ packages/server/src/routes/api.ts
@@ -3,7 +3,7 @@ import { logger } from 'hono/logger';
 import Dictionaries from './dictionary';
 import Token from './getToken';
 import Tts from './tts';
-import Websocket from './websocket'
+
 
 const app = new Hono();
 app.use('*', logger());
@@ -11,6 +11,6 @@ app.use('*', logger());
 app.route('/', Tts);
 app.route('/', Token);
 app.route('/', Dictionaries);
-app.route('/', Websocket);
+
 
 export default app;
